43ab0b960acab9b5c5aa0873e8320837bc693733
Corina Ramont <corinaramont@tamu.edu>
2024-04-05 15:41:12 -0500

    add comments, run rf on only observations w/ no missing data

--- corina/explore.R
+++ corina/explore.R
@@ -27,3 +27,30 @@ ggplot_missing = function(x){
          y = "Rows / observations")
 } 
 ggplot_missing(data)
+
+miss_data = data[rowSums(is.na(data)) > 0, ]
+remain_data = data[rowSums(is.na(data)) == 0,]
+
+# Attempt at random forest if all observations with missing values removed -----
+set.seed(656)
+library(randomForest)
+library(caret)
+
+## partition 80% into training and 20% into testing
+train_index = createDataPartition(remain_data$LoanStatus, p = .8, list = FALSE) %>% 
+  as.vector(.)
+train_data = (remain_data[train_index,])[,-c(1,2)]
+test_data = (remain_data[-train_index,])[,-c(1,2)]
+
+rf = randomForest(factor(LoanStatus) ~ ., data = train_data, ntree = 1000)
+print(rf)
+
+# plot all 4 types of error
+oob_data = data.frame(
+  trees = rep(1:nrow(rf$err.rate), 4), 
+  type = rep(c("OOB","Chargedoff","Completed","Defaulted"), 
+             each = nrow(rf$err.rate)),
+  error = c(rf$err.rate[,"OOB"], rf$err.rate[,"Chargedoff"], 
+            rf$err.rate[,"Completed"], rf$err.rate[,"Defaulted"]))
+ggplot(data = oob_data, aes(x = trees, y= error)) + 
+  geom_line(aes(color = type))
