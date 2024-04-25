29335e39e3d0e2c3a735a836219e0612f2473c4d
dylanward01 <108138384+dylanward01@users.noreply.github.com>
2024-04-25 16:09:21 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -8,16 +8,18 @@ library(glmnet)
 library(gridExtra)
 library(caret)
 library(randomForest)
+library(grid)
+
 
 # Set up Layout for Application
 ui <- fluidPage(
   #theme = bslib::bs_theme(bootswatch = "pulse"),
-  useShinyjs(),
+  shinyjs::useShinyjs(),
   tags$h1("STAT 656 Project"),
   #tags$a(tags$strong(tags$em("Source: Empirical Frequency Band Analysis of Nonstationary Time Series")), 
   #       href = "https://www.tandfonline.com/doi/full/10.1080/01621459.2019.1671199"),
   tags$p(tags$em("This project aims at seeing if Loan Status in the Prosper Loan dataset can be predicted. We will do this
-                 through both Random Forest Models, and Penalized Regression. The dataset consists of 55058 total observations and 66 features,
+                 through both Penalized Regression and Random Forest Models. The dataset consists of 55058 total observations and 66 features,
                  a number of which were only recorded after 2009. As such, you can fit a model on either pre 2009
                  data, or post 2009 data.")),
   tags$p(tags$strong("Authors: Dylan Ward, Megan Tung, Corina Ramont")),
@@ -31,7 +33,7 @@ ui <- fluidPage(
                        radioButtons("prTrain", label = 'Do you want to utilize Tuned Parameters, or to select your own?',
                                     choices = c("Have Tuning Done to Select Parameters", 
                                                 "Manually Select Parameters"),
-                                                selected = "Manually Select Parameters"),
+                                                selected = "Have Tuning Done to Select Parameters"),
                        # Inputs for Simulated, Univariate Data
                        numericInput(inputId = "alpha", label = "Select the Alpha you wish to use *", 
                                     value = 0.5, step = 0.01, min=0, max=1), # Sets value for N
@@ -50,8 +52,7 @@ ui <- fluidPage(
                        radioButtons("rfTrain", label = 'Do you want to utilize Tuned Parameters, or to select your own?',
                                     choices = c("Have Tuning Done to Select Parameters", 
                                                 "Manually Select Parameters"),
-                                    selected = "Manually Select Parameters"),
-                       
+                                    selected = "Have Tuning Done to Select Parameters"),
                        selectInput(inputId = "ntree", label = "Choose the number of trees to grow", 
                                     choices = c(100,250,500,1000,2000,5000), selected = 1000), # Sets value for N
                        selectInput(inputId = "mtry", label = "Choose the number of variables randomly sampled at each split", 
@@ -84,7 +85,7 @@ ui <- fluidPage(
                        plotOutput("Image_Plot2", height=600, width=1000), # Plots local periodogram
                        plotOutput("Image_Plot3", height=600, width=1000),
                        plotOutput("Image_Plot4", height=200, width=1000),
-                       plotOutput("Image_Plot5", height=600,width=1000),
+                       plotOutput("Image_Plot5", height=600, width=1000),
                        plotOutput("Image_Plot6", height=400, width=1000),
                        hidden(downloadButton('downloadData1','Download the Above Results')), # Downloads the plots seen into a pdf
                        br(),
@@ -104,31 +105,19 @@ server <- function(input,output, session) {
   output$Res1 <- renderText({
     paste(h6("** Values were chosen to be 30 equally spaced values between 0 and 0.1"))
   })
-  observe( {
-    trainVal <- input$prTrain
-    if(input$prTrain == "Have Tuning Done to Select Parameters") {
-      hide("alpha")
-      hide("lambda")
-    } else {
-      show("alpha")
-      show("lambda")
-    }
+  observeEvent(input$prTrain, {
+    toggle("alpha")
+    toggle("lambda")
   })
-  observe( {
-    trainVal <- input$rfTrain
-    if(input$rfTrain == "Have Tuning Done to Select Parameters") {
-      hide("ntree")
-      hide("mtry")
-    } else {
-      show("ntree")
-      show("mtry")
-    }
+  observeEvent(input$rfTrain, {
+      toggle("ntree")
+      toggle("mtry")
   })
   
   ### Start Penalized Regression Block
   
   plot.listPR <- eventReactive(input$go, ignoreNULL = TRUE, {
-    data <- read_csv("https://raw.githubusercontent.com/corinaramont/stat656_proj/main/CleanedProsperData.csv")[,-1]
+    data <- read.csv("CleanedProsperData (1).csv")[,-1]
     data$ListingCreationDate <- as.character(data$ListingCreationDate)
     data$BorrowerState <- factor(data$BorrowerState)
     data$ListingKey <- NULL
@@ -188,7 +177,7 @@ server <- function(input,output, session) {
     xTest <- xTest[,classes=="numeric"]    
     training <- input$prTrain
     if(input$prTrain == "Have Tuning Done to Select Parameters"){
-      show("Image_Plot")
+      shinyjs::show("Image_Plot")
       K            = 10
       trainControl = trainControl(method = "cv", number = K)
       tuneGrid     = expand.grid('alpha'=c(0,.25,.5,.75,1),'lambda' = seq(00, .001, length.out = 30))
@@ -293,7 +282,7 @@ server <- function(input,output, session) {
   #### Start Random Forest Block ####
   
   plot.listRF <- eventReactive(input$go2, ignoreNULL = TRUE, {
-    data <- read_csv("https://raw.githubusercontent.com/corinaramont/stat656_proj/main/CleanedProsperData.csv")[,-1]
+    data <- read.csv("CleanedProsperData (1).csv")[,-1]
     data$ListingCreationDate <- as.character(data$ListingCreationDate)
     currTime <- numeric(0)
     for(i in 1:length(data$ListingCreationDate)){
@@ -316,13 +305,14 @@ server <- function(input,output, session) {
       data$CreditGrade <- NULL
     }
     set.seed(656)
+    remain_data = data[rowSums(is.na(data)) == 0,]
     train_index = createDataPartition(remain_data$LoanStatus, p = .8, list = FALSE) %>% 
       as.vector(.)
     train_data = (remain_data[train_index,])[,-c(1,2)]
     test_data = (remain_data[-train_index,])[,-c(1,2)]
     if(input$rfTrain == "Have Tuning Done to Select Parameters"){
-      show("Image_Plot2")
-      show("Image_Plot3")
+      shinyjs::show("Image_Plot2")
+      shinyjs::show("Image_Plot3")
       train = "Yes"
       ntree = 5000
       mtry = floor(sqrt(ncol(train_data) - 1))
@@ -344,11 +334,11 @@ server <- function(input,output, session) {
                  each = nrow(rf$err.rate)),
       error = c(rf$err.rate[,"OOB"], rf$err.rate[,"Chargedoff"], 
                 rf$err.rate[,"Completed"], rf$err.rate[,"Defaulted"]))
-    tuning <- tuneRF(train_data[,-1], factor(train_data$LoanStatus), nTreeTry = 1000, 
-                  stepFactor=1.5,improve=0.01,trace=FALSE, plot=FALSE,
-                  sampsize = c('Chargedoff' = 300, 'Completed' = 300,
-                               'Defaulted' = 300))
     if(input$rfTrain == "Have Tuning Done to Select Parameters"){
+      tuning <- tuneRF(train_data[,-2], factor(train_data$LoanStatus), nTreeTry = 1000, 
+                       stepFactor=1.5,improve=0.01,trace=FALSE, plot=FALSE,
+                       sampsize = c('Chargedoff' = 300, 'Completed' = 300,
+                                    'Defaulted' = 300))
       ntreeNew = which.min(oob_data$error[1:5000])
       mtryNew = unname(tuning[,1][which.min(tuning[,2])])
       rf1 = randomForest(factor(LoanStatus) ~ ., data = train_data, 
@@ -356,6 +346,7 @@ server <- function(input,output, session) {
                                       'Defaulted' = 300), 
                          ntree = ntreeNew, mtry=mtryNew)
     } else {
+      tuning <- 1:100
       ntreeNew = ntree
       mtryNew = mtry
     }
@@ -388,7 +379,7 @@ server <- function(input,output, session) {
       abline(v = as.numeric(plot.listRF()[[4]]),col = rgb(0.49, 0.81, 0.54))
     })
     output$Image_Plot3 <- renderPlot({
-      plot(plot.listRF()[[9]], type="l")
+      plot(plot.listRF()[[9]], type="l", main = "OOB Error for various values of mtry")
       abline(v = as.numeric(plot.listRF()[[5]]),col = rgb(0.49, 0.81, 0.54))
     })
     output$Image_Plot4 <- renderPlot({
@@ -398,7 +389,12 @@ server <- function(input,output, session) {
       vals <- c(ntree, mtry)
       res <- data.frame("Metric" = Metric, "Value" = vals)
       res1 <- tableGrob(res, rows = NULL)
-      title <- textGrob(expression(bold("Selected ntree and mtry Values for Model Fitting")))
+      if(plot.listRF()[[6]] == "Yes"){
+        title <- textGrob(expression(bold("Selected ntree and mtry Values, which minimized OOB Error, for Model Fitting")))
+      } else {
+        title <- textGrob(expression(bold("Chosen ntree and mtry Values, for Model Fitting")))
+        
+      }
       blank9090 <- textGrob(""); blank0909 <- textGrob("")
       grid.arrange(blank9090,title, res1, blank0909, ncol = 1)
     })
